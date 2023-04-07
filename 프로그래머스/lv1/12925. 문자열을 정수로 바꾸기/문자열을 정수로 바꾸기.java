ed1aae49e5e13a1ade2303018fe88a9b1e50484b
김근보 <125139072+CaptainGombo@users.noreply.github.com>
2023-04-08 01:18:33 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
+//
+//Integer.parseInt("문자열")
+//Double.parseDouble("문자열")
+//Float.parseFloat()
+// String -> int
+//parse를 사용하면 String타입을 숫자형 타입으로 바꿀 수 있다.
