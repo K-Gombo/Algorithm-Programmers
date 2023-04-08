8ba7d5cfec73178de16b341d6840fe4c1b6a55a3
김근보 <125139072+CaptainGombo@users.noreply.github.com>
2023-04-08 11:39:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 304.95 ms, Memory: 95.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        int i;
+        for(i = 0; i<completion.length;i++){
+            if(!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+        return participant[participant.length-1];
+        
+     }
+
+}
+
+//Arraysort 로 각 배열 정렬
+//for문을 사용하여 원소들을 비교해서 완주자 배열에있는 같지 않은 사람을 출력
