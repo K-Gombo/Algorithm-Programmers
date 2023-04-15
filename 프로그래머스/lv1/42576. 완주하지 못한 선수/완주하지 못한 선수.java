15f15256303b20d09997a72f19b856b980638ab3
김근보 <125139072+CaptainGombo@users.noreply.github.com>
2023-04-15 11:07:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 320.65 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
+       
         Arrays.sort(participant);
         Arrays.sort(completion);
 
